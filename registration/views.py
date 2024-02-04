0c7006c3ad13f4c3acc1ea5feea3e669186c5c72
AceYang123 <leonisita@126.com>
2024-02-03 19:52:41 -0500

    debugged the csrf, and changed another way for token

--- registration/views.py
+++ registration/views.py
@@ -37,6 +37,11 @@ def register_user(request):
         username = request.POST.get('Username', '')
         password = request.POST.get('Password',
                                     '')  # 这个版本更加安全，如果没有password，default会用''
+        if User.objects.filter(username=username).exists():
+            error = {"error": "Username already exists"}
+            messages = ('FAILED: Account creation failed - Username already '
+                        'exists')
+            return send_response(409, messages, error)
         if username != '' and password != '':
             User.objects.create_user(username=username, password=password)
             messages = f'Account created for {username}!'
