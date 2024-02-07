253006fb7b9bfad14680ebca5d05ba52eaacf9b3
AceYang123 <leonisita@126.com>
2024-02-07 02:36:55 -0500

    change settings.py

--- playpal_backend/settings.py
+++ playpal_backend/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'registration',
-    'rest_framework',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
