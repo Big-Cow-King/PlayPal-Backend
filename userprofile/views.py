57a458f455eec27bd83c94cb973e07fb2e66524d
ZHCC <hsz@zhccs-MacBook-Pro.local>
2024-02-06 20:26:54 -0500

    add Authenticated to views

--- userprofile/views.py
+++ userprofile/views.py
@@ -1,23 +1,30 @@
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Profile
 from .serializers import ProfileSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
-# @login_required  # ============ Uncomment if need login=================
 class EditProfileView(APIView):
-    def post(self, request):
-        serializer = ProfileSerializer(request.user.profile, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        try:
+            profile_instance = request.user.profile
+            serializer = ProfileSerializer(profile_instance, data=request.data, partial=True)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+        except ObjectDoesNotExist:
+            return Response({'error': 'Profile not found.'}, status=status.HTTP_404_NOT_FOUND)
 
 
-# @login_required  # ============ Uncomment if need login=================
 class ProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
     def get(self, request, *args, **kwargs):
         try:
             profile = request.user.profile
