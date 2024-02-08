be3a791adcc81afd27ed329bb5f6de1f9e9f502e
Feiran Huang <philipp.huang@mail.utoronto.ca>
2024-02-08 16:51:16 -0500

    #31 Fixed event create required not necessary field

--- events/serializers.py
+++ events/serializers.py
@@ -14,8 +14,8 @@ class AttachmentSerializer(serializers.ModelSerializer):
         return Attachment.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.file = validated_data.get('file', instance.file)
+        instance.name = validated_data.get('name')
+        instance.file = validated_data.get('file')
         instance.save()
         return instance
 
@@ -29,33 +29,37 @@ class SportSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         return Sport.objects.create(**validated_data)
 
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.save()
-        return instance
-
 
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
         fields = '__all__'
-        read_only_fields = ['id', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'created_at', 'updated_at', 'owner', 'admins', 'players']
         sports = SportSerializer(many=True)
 
     def create(self, validated_data):
         created_at = timezone.now()
         updated_at = timezone.now()
-        attachments = validated_data.pop('attachments', [])
+        attachments = validated_data.pop('attachments')
+        sports = validated_data.pop('sports')
         event = Event.objects.create(created_at=created_at,
-                                     updated_at=updated_at, **validated_data)
+                                     updated_at=updated_at,
+                                     owner=self.context['request'].user,
+                                     **validated_data)
         for attachment in attachments:
             Attachment.objects.create(event=event, **attachment)
+        for name in sports:
+            name = name.lower()
+            try:
+                sport = Sport.objects.get(name=name)
+            except Sport.DoesNotExist:
+                sport = Sport.objects.create(name=name)
+            event.sports.add(sport)
         return event
 
     def update(self, instance, validated_data):
         instance.updated_at = timezone.now()
-        instance.start_time = validated_data.pop('start_time',
-                                                 instance.start_time)
+        instance.start_time = validated_data.pop('start_time', instance.start_time)
         instance.end_time = validated_data.pop('end_time', instance.end_time)
         instance.title = validated_data.pop('title', instance.title)
         instance.description = validated_data.pop('description',
@@ -63,23 +67,24 @@ class EventSerializer(serializers.ModelSerializer):
         instance.content = validated_data.pop('content', instance.content)
         instance.max_players = validated_data.pop('max_players',
                                                   instance.max_players)
-        sports = validated_data.pop('sports', instance.sports)
+        sports = validated_data.pop('sports')
         if sports:
-            for sport in sports:
+            for name in sports:
+                name = name.lower()
                 try:
-                    sport = Sport.objects.get(name=sport)
+                    sport = Sport.objects.get(name=name)
                 except Sport.DoesNotExist:
-                    sport = Sport.objects.create(name=sport)
-                instance.sports.add(sport)
+                    sport = Sport.objects.create(name=name)
+                instance.sports.set(sport)
 
         attachments = validated_data.pop('attachments', [])
         if attachments:
             for attachment in attachments:
                 Attachment.objects.create(event=instance, **attachment)
 
-        instance.players.add(
-            *validated_data.get('players', instance.players.all()))
-        instance.admins.add(
-            *validated_data.get('admins', instance.admins.all()))
+        instance.players.set(
+            validated_data.get('players', instance.players.all()))
+        instance.admins.set(
+            validated_data.get('admins', instance.admins.all()))
         instance.save()
         return instance
