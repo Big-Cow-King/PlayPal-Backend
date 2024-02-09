239419148cfbf4ad03fd8d9f22aa8d258572103d
Feiran Huang <philipp.huang@mail.utoronto.ca>
2024-02-08 19:10:59 -0500

    #34 Now event only allow uploading 1 image

--- events/models.py
+++ events/models.py
@@ -27,14 +27,21 @@ class Event(models.Model):
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
     title = models.CharField(max_length=100)
-    attachment = models.ImageField(upload_to='static/events/attachments/')
+    attachment = models.ImageField(upload_to='static/events/attachments/',
+                                   null=True, blank=True)
     description = models.TextField(max_length=50)
     content = models.TextField(max_length=1000)
-    sport = models.ForeignKey(Sport, on_delete=models.CASCADE, related_name='events')
-    players = models.ManyToManyField('auth.User') # TODO: Need to be edit after merging with user app
+    sport = models.ForeignKey(Sport, on_delete=models.CASCADE,
+                              related_name='events', null=True, blank=True)
+    players = models.ManyToManyField('auth.User', null=True,
+                                     blank=True)  # TODO: Need to be edit after merging with user app
     level = models.TextField(choices=level_choices.items())
     age_group = models.TextField(choices=age_group_choices.items())
     max_players = models.IntegerField()
-    owner = models.ForeignKey('auth.User', related_name='events', on_delete=models.CASCADE) # TODO: Need to be edited after merging with user app
-    admins = models.ManyToManyField('auth.User', related_name='admin_events') # TODO: Need to be edited after merging with user app
+    owner = models.ForeignKey('auth.User', related_name='events',
+                              on_delete=models.CASCADE, null=True, blank=True,
+                              default=None)  # TODO: Need to be edited after merging with user app
+    admins = models.ManyToManyField('auth.User', related_name='admin_events',
+                                    null=True,
+                                    blank=True)  # TODO: Need to be edited after merging with user app
     location = models.CharField(max_length=100)
