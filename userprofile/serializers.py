e321f41e12ff536229437fba3a390503e7b395aa
ZHCC <hsz@zhccs-MacBook-Pro.local>
2024-02-09 03:42:14 -0500

    #3 update code for serializer

--- userprofile/serializers.py
+++ userprofile/serializers.py
@@ -1,3 +1,5 @@
+import base64
+from django.core.files.base import ContentFile
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Profile
@@ -12,22 +14,38 @@ class UserSerializer(serializers.ModelSerializer):
 class ProfileSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
     email = serializers.EmailField(source='user.email')
+    avatar_data = serializers.CharField(write_only=True, required=False, allow_blank=True, allow_null=True)
 
     class Meta:
         model = Profile
-        fields = ('id', 'user', 'name', 'email', 'gender', 'sports_you_can_play',
+        fields = ('user', 'name', 'email', 'gender', 'sports_you_can_play',
                   'phone_no', 'age', 'description', 'avatar',
-                  'email_product', 'email_security', 'phone_security')
+                  'email_product', 'email_security', 'phone_security', 'avatar_data')
 
     def update(self, instance, validated_data):
-        user_data = validated_data.pop('user', {})
         user = instance.user
-
-        user.email = user_data.get('email', user.email)
+        user.email = validated_data.get('email', user.email)
+        user.password = validated_data.get('password', user.password)
         user.save()
+        instance.name = validated_data.get('name', instance.name)
+        instance.gender = validated_data.get('gender', instance.gender)
+        instance.sports_you_can_play = validated_data.get('sports_you_can_play', instance.sports_you_can_play)
+        instance.phone_no = validated_data.get('phone_no', instance.phone_no)
+        instance.age = validated_data.get('age', instance.age)
+        instance.description = validated_data.get('description', instance.description)
+        instance.email_product = validated_data.get('email_product', instance.email_product)
+        instance.email_security = validated_data.get('email_security', instance.email_security)
+        instance.phone_security = validated_data.get('phone_security', instance.phone_security)
+        try:
+            avatar_data = validated_data.pop('avatar_data')
+            if avatar_data:
+                format, imgstr = avatar_data.split(';base64,')
+                ext = format.split('/')[-1]
+                data = ContentFile(base64.b64decode(imgstr), name=f'{instance.user.username}.{ext}')
+                instance.avatar = data
+            else:
+                instance.avatar = None
+        except KeyError:
+            pass
 
-        for attr, value in validated_data.items():
-            setattr(instance, attr, value)
         instance.save()
-
-        return instance
--- userprofile/tests.py
+++ userprofile/tests.py
@@ -1,3 +1,50 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
+from .models import Profile
 
-# Create your tests here.
+
+class ProfileTestCase(APITestCase):
+    def setUp(self):
+        # Create a user and associated profile for testing
+        self.user = User.objects.create_user(username='testuser', email='test@example.com', password='testpassword123')
+        Profile.objects.create(user=self.user, phone_no='1234567890', name='Test User', gender='Male', age=25,
+                               description='Original description', sports_you_can_play='Tennis')
+
+        # URL for getting the profile and editing the profile
+        self.get_profile_url = reverse('profile_view')  # Corrected URL name
+        self.edit_profile_url = reverse('edit_profile')  # Corrected URL name
+
+        # Log in the user
+        self.client.login(username='testuser', password='testpassword123')
+
+    def test_retrieve_profile(self):
+        # Attempt to retrieve the profile
+        response = self.client.get(self.get_profile_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['phone_no'], '1234567890')
+        self.assertEqual(response.data['name'], 'Test User')
+
+    def test_update_profile(self):
+        # Data to update the profile
+        data = {
+            'name': 'Updated Name',
+            'phone_no': '0987654321',
+            'gender': 'Female',
+            'age': 30,
+            'description': 'Updated description',
+            'sports_you_can_play': 'Soccer, Tennis',
+            'email_product': True
+        }
+
+        # Attempt to update the profile
+        response = self.client.post(self.edit_profile_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # Refetch the profile to verify updates
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.profile.name, 'Updated Name')
+        self.assertEqual(self.user.profile.phone_no, '0987654321')
+
+
