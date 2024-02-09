1bcfc8e824524766f5421ada57cb2903842b80bd
ZHCC <hsz@zhccs-MacBook-Pro.local>
2024-02-09 13:55:32 -0500

    #3 implement get user profiles

--- userprofile/models.py
+++ userprofile/models.py
@@ -3,9 +3,9 @@ from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255, blank=True, null=True)
-    gender = models.CharField(max_length=10, blank=True, null=True)
+    gender = models.CharField(max_length=40, blank=True, null=True)
     sports_you_can_play = models.TextField(blank=True, null=True)
     phone_no = models.CharField(max_length=20, blank=True, null=True)
     age = models.IntegerField(blank=True, null=True)
